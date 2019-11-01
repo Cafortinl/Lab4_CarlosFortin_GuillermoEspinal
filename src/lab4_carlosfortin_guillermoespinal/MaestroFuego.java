d5c58028c9873289df2d9a18260ad23e589c1401
Guillermo Espinal <guille.gaes@gmail.com>
2019-11-01 13:56:54 -0600

    Clases de los dos ultimos maestros

---
+++ src/lab4_carlosfortin_guillermoespinal/MaestroFuego.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_carlosfortin_guillermoespinal;
+
+/**
+ *
+ * @author admin
+ */
+public class MaestroFuego extends Mestro{
+
+    public MaestroFuego() {
+        
+    }
+
+    public MaestroFuego(String nombre, String rango, int edad) {
+        super(nombre, rango, edad);
+        super.setPtos_Vida(160);
+        super.setAtaque1(40);
+        super.setAtaque2(45);
+    }
+    
+
+    @Override
+    public String toString() {
+        return "MaestroFuego{" + '}';
+    }
+    public int Ataque1 (){
+        return super.getAtaque1();
+    }
+    public int Ataque2(){
+        return super.getAtaque2();
+    }
+}
---
+++ src/lab4_carlosfortin_guillermoespinal/MaestroTierra.java
@@ -0,0 +1,28 @@
+
+package lab4_carlosfortin_guillermoespinal;
+
+
+public class MaestroTierra extends Mestro{
+
+    public MaestroTierra() {
+    }
+
+    public MaestroTierra(String nombre, String rango, int edad) {
+        super(nombre, rango, edad);
+        super.setPtos_Vida(200);
+        super.setAtaque1(40);
+        super.setAtaque2(52);
+    }
+
+    @Override
+    public String toString() {
+        return "MaestroTierra{" + '}';
+    }
+    
+    public int Ataque1(){
+        return super.getAtaque1();
+    }
+    public int Ataque2(){
+        return super.getAtaque2();
+    }
+}
